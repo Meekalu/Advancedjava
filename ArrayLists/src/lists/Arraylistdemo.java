7841ff7481731432169df2e0a991254c5f1be73f
Meekalu <78653690+Meekalu@users.noreply.github.com>
2021-02-06 21:26:54 +0530

    EXP1

---
+++ ArrayLists/src/lists/Arraylistdemo.java
@@ -0,0 +1,28 @@
+package lists;
+
+import java.util.ArrayList;
+
+public class Arraylistdemo {
+	
+	public static void main(String[] args) {
+		
+		
+		ArrayList <String> stlist = new ArrayList <String>();
+		
+		
+		
+		stlist.add("meekalu");
+		stlist.add("Mounika");
+		stlist.add("Sirisha");
+		stlist.add("Deevena");
+		stlist.add("Mercy");
+		stlist.add("Unice");
+		
+		
+	System.out.println(stlist);
+		
+		
+		
+	}
+
+}
