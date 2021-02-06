331dfb18195ec437b2af9ec840a256afdb504dd3
Meekalu <78653690+Meekalu@users.noreply.github.com>
2021-02-06 21:38:29 +0530

    exp1(a)

---
+++ Arraylist/src/lists/Question1.java
@@ -0,0 +1,14 @@
+package lists;
+
+public class Question1 {
+	
+	public static void main(String[] args) {
+		
+		
+		Arraylists <String> stlist = new Arraylists <String>();
+		
+		stlist.add("Meekalu");
+		stlist.add("Mounika")
+	}
+
+}
